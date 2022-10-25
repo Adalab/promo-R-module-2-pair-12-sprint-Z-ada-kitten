a66391f10a1dc41d6e4e977b8d2fef933de46bf2
Ursula Borrego <ursulaborrego@hotmail.com>
2022-10-25 15:28:34 +0100

    do javascript organization

--- js/main.js
+++ js/main.js
@@ -1,75 +1,47 @@
 'use strict';
-const inputSearchDesc = document.querySelector('.js_in_search_desc');
-const inputSearchRace = document.querySelector('.js_in_search_race');
-const btnSearch = document.querySelector(".js-btn_search");
-const labelMesageError2 = document.querySelector('.js-label-error2');
+
+// 1- QUERYSELECTOR
+
 const icon = document.querySelector('.js-icon-plus');
+const openForm = document.querySelector('.js-openform');
+// Estos dos querySelector son para la misma etiqueta - QUITAR UNO
 
-// input_search_desc.value = 'cariñoso';
-// const descrSearchText = input_search_desc.value;
-
-const list = document.querySelector(".js-list");
-const btnAdd = document.querySelector(".js-btn-add");
-
-const inputDesc = document.querySelector('.js-input-desc');
+//Form - ¿Agregamos un nuevo gatito a Adalab?
+const form = document.querySelector('.js-new-form');
 const inputPhoto = document.querySelector('.js-input-photo');
 const inputName = document.querySelector('.js-input-name');
+const inputDesc = document.querySelector('.js-input-desc');
 const labelMesageError = document.querySelector('.js-label-error');
+const btnAdd = document.querySelector(".js-btn-add");
 
-btnSearch.addEventListener('click', (event) =>{
-  event.preventDefault();
-  const valueSearchDesc = inputSearchDesc.value;
-  const valueSearchRace = inputSearchRace.value;
-  if (valueSearchDesc === '' || valueSearchRace === '') {
-    labelMesageError2.innerHTML="Debe rellenar todos los valores"
-  } else {
-  }
-});
+//Form - Filtrar/Buscar
+const inputSearchDesc = document.querySelector('.js_input_search_desc');
+const inputSearchRace = document.querySelector('.js_input_search_race');
+const btnSearch = document.querySelector(".js-btn_search");
+const labelMesageError2 = document.querySelector('.js-label-error2');
 
-function showNewCatForm() {
-  form.classList.remove('collapsed');
-}
-function hideNewCatForm() {
-  form.classList.add('collapsed');
-}
+//Ul - Lista gatos
+const list = document.querySelector(".js-list");
 
-//IF PERO SIN LLAMAR A LAS FUNCIONES
-icon.addEventListener("click", (event) =>{
-  if (form.classList.contains('collapsed')) {
-    form.classList.remove('collapsed');
-    } else {
-    form.classList.add("collapsed");
-    }
-});
+
+// 2 - VARIABLES
 
 const kittenOneImage = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
 const kittenOneName = 'Anastacio';
 const kittenOneDesc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
 const kittenOneRace = 'British Shorthair';
-const openForm = document.querySelector('.js-openform')
 
-const form = document.querySelector('.js-new-form');
-openForm.addEventListener('click', (event) =>{
-event.preventDefault();
-  if (form.classList.contains('collapsed')) {
-   form.classList.remove('collapsed');
-  } else {
-  form.classList.add("collapsed");
-  }
-});
+const kittenTwoImage = 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg';
+const kittenTwoName = 'Fiona';
+const kittenTwoDesc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
+const kittenTwoRace = 'British Shorthair';
 
-btnAdd.addEventListener('click', (event) =>{
-  event.preventDefault();
-  const valueDesc = inputDesc.value;
-  const valuePhoto = inputPhoto.value;
-  const valueName = inputName.value;
-  if (valueDesc === '' || valuePhoto === '' || valueName === '') {
-    labelMesageError.innerHTML="Debe rellenar todos los valores"
-  } else {
-  }
-});
+const kittenThreeImage = 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg';
+const kittenThreeName = 'Cielo';
+const kittenThreeDesc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
+const kittenThreeRace = 'British Shorthair';
 
-
+// "Llamadas" a las variables
 const kittenOne = `<li class="card">
 <article>
   <img
@@ -85,15 +57,6 @@ const kittenOne = `<li class="card">
 </article>
 </li>`;
 
-if( kittenOneDesc.includes(descrSearchText) ) {
-  kittenOneDesc.classList.remove("collapsed");
-}
-
-const kittenTwoImage = 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg';
-const kittenTwoName = 'Fiona';
-const kittenTwoDesc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
-const kittenTwoRace = 'British Shorthair';
-
 const kittenTwo = `<li class="card">
 <img
   class="card_img"
@@ -107,16 +70,6 @@ const kittenTwo = `<li class="card">
 </p>
 </li>`;
 
-if( kittenTwoDesc.includes(descrSearchText) ) {
-  kittenTwoDesc.classList.remove("collapsed");
-}
-
-const kittenThreeImage = 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg';
-const kittenThreeName = 'Cielo';
-const kittenThreeDesc =
- 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
-const kittenThreeRace = 'British Shorthair';
-
 const kittenThree = `<li class="card">
 <img
   class="card_img"
@@ -129,8 +82,76 @@ const kittenThree = `<li class="card">
 </p>
 </li>`;
 
-if( kittenThreeDesc.includes(descrSearchText) ) {
-  kittenThreeDesc.classList.remove("collapsed");
+list.innerHTML = kittenOne + kittenTwo + kittenThree;
+
+
+// 3 - FUNCIONES
+
+// Mostrar/Esconder NewCat
+function showNewCatForm() {
+  form.classList.remove('collapsed');
+}
+function hideNewCatForm() {
+  form.classList.add('collapsed');
 }
 
-list.innerHTML = kittenOne + kittenTwo + kittenThree;
+
+// 4 - ADDEVENTLISTENER
+
+//If pero sin llamar a las funciones => NOS QUEDAMOS POR AQUÍ 25/10
+icon.addEventListener("click", (event) =>{
+  if (form.classList.contains('collapsed')) {
+    form.classList.remove('collapsed');
+    } else {
+    form.classList.add("collapsed");
+    }
+});
+
+openForm.addEventListener('click', (event) =>{
+  event.preventDefault();
+  if (form.classList.contains('collapsed')) {
+     form.classList.remove('collapsed');
+  } else {
+    form.classList.add("collapsed");
+  }
+});
+//VOLVEMOS A TENER LA MISMA ETIQUETA 2 VECES, LLAMADA CON ADDEVENTLISTENER 2 VECES
+
+btnAdd.addEventListener('click', (event) =>{
+  event.preventDefault();
+  const valueDesc = inputDesc.value;
+  const valuePhoto = inputPhoto.value;
+  const valueName = inputName.value;
+  if (valueDesc === '' || valuePhoto === '' || valueName === '') {
+    labelMesageError.innerHTML="Debe rellenar todos los valores"
+  } else {
+  }
+});
+
+btnSearch.addEventListener('click', (event) =>{
+  event.preventDefault();
+  const valueSearchDesc = inputSearchDesc.value;
+  const valueSearchRace = inputSearchRace.value;
+  if (valueSearchDesc === '' || valueSearchRace === '') {
+    labelMesageError2.innerHTML="Debe rellenar todos los valores"
+  } else {
+  }
+});
+
+// input_search_desc.value = 'cariñoso'; //Esto sigue dando Error y no sé si va aquí
+// const descrSearchText = input_search_desc.value;
+
+// // If kittenOne
+// if( kittenOneDesc.includes(descrSearchText) ) {
+//   kittenOneDesc.classList.remove("collapsed");
+// }
+
+// // IfkittenTwo
+// if( kittenTwoDesc.includes(descrSearchText) ) {
+//   kittenTwoDesc.classList.remove("collapsed");
+// }
+
+// // If kittenThree
+// if( kittenThreeDesc.includes(descrSearchText) ) {
+//   kittenThreeDesc.classList.remove("collapsed");
+// }
